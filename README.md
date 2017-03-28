8878c0e859c00ceed30d89f8841290d24ff4c25b
Dauzy <daniel_6287@hotmail.com>
2017-03-28 09:53:17 -0600

    Introduction Quiz Answers

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 **Introduction Quiz**
 >Install
   1. ``` git clone https://github.com/Dauzy/introduction-quiz.git ```
-  2. ``` pip install requests ``` 
-  3. ``` python send_answers.py ```
+  2. ``` python3 -m venv env ``` 
+  3. ``` source env/bin/activate ```
+  4. ``` pip install -r requirements.txt ``` 
+  5. ``` python send_answers.py ```
+  6. si deseas remplazar la url, en la variable url, cambia 'localhost'
+
--- send_answers.py
+++ send_answers.py
@@ -1,12 +1,11 @@
 import requests
 import json
-url = "http://locahost:80"
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-quiz_json = 'introduction_quiz.json'
-respuestas = open(quiz_json)
-data = json.load(respuestas)
 
-r = requests.post(url, data=json.dumps(data), headers=headers)
-r.status_code
-r.json()
-print(data)
+url = "http://locahost:9200/beeva/quiz?pretty"
+quiz_json = 'introduction_quiz.json'
+with open('introduction_quiz.json') as data_file:
+	data = json.load(data_file)
+
+response = requests.post(url, json=data)
+results = json.loads(response.text)
+print(response.text)
